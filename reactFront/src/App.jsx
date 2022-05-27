3bb6d5cee11ddc6eefdb95ff4d4e95179fd4b748
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-05-26 23:52:08 -0300

    conexion ready

--- reactFront/src/App.jsx
+++ reactFront/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Home from './components/home/Home'
+import Nav from './components/nav/Nav'
 
 function App() {
   return (
     <>
-      
+      <Nav />
+      <Home />
     </>
   )
 }
