89ce0b2e0180510e0b8c7a8600bed437cc5e465e
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-05-29 23:44:10 -0300

    mini changes

--- reactFront/src/blog/ShowBlog.jsx
+++ reactFront/src/blog/ShowBlog.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import "./showBlog.css"
 
-const URL = "http://localhost:8000/blogs/"
+const URI = "http://localhost:8000/blogs/"
 
 const CompShowBlog = () => {
   const [blogs, setBlogs] = useState([])
@@ -13,7 +13,7 @@ const CompShowBlog = () => {
 
   // procedimiento para obtener los blogs
   const getBlogs = async () => {
-    const res = await axios.get(URL)
+    const res = await axios.get(URI)
     setBlogs(res.data)
   }
 
@@ -28,7 +28,7 @@ const CompShowBlog = () => {
       <h1>App full Stack</h1>
       <div className="row">
         <div className="col">
-          <Link to={"/create"} className="btn btn-primary mt-2 mb-2">Crear</Link>
+          <Link to={"/create"} className="btn btn-primary mt-2 mb-4">Crear</Link>
           <table className="table">
             <thead>
               <tr>
@@ -43,6 +43,7 @@ const CompShowBlog = () => {
                   <td>{blog.title}</td>
                   <td>{blog.content}</td>
                   <td>
+                    <Link to={`/edit/${blog.id}`} className="btn btn-warning">Edit</Link>
                     <button
                       className="btn btn-danger"
                       onClick={() => deleteBlog(blog.id)}
